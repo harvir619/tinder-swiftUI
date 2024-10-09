247bcaf6f5aaec843c43f44d6bce901c7f8b24be
Harvir Brar <83703941+harvir619@users.noreply.github.com>
2024-10-09 18:05:12 +0800

    Initial Commit

---
+++ tinder/tinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  tinderApp.swift
+//  tinder
+//
+//  Created by Harvir Brar on 9/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct tinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
